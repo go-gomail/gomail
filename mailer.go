74f94c0f269a90288aaa0395d640f3a288c54b2c
Alexandre Cesaro <alexandre.cesaro@gmail.com>
2015-03-17 15:40:16 +0100

    Improved memory consumption
    Closes #25

--- mailer.go
+++ mailer.go
@@ -129,11 +129,17 @@ func flattenHeader(msg *mail.Message, bcc string) []byte {
 
 	for field, value := range msg.Header {
 		if field != "Bcc" {
-			buf.WriteString(field + ": " + strings.Join(value, ", ") + "\r\n")
+			buf.WriteString(field)
+			buf.WriteString(": ")
+			buf.WriteString(strings.Join(value, ", "))
+			buf.WriteString("\r\n")
 		} else if bcc != "" {
 			for _, to := range value {
 				if strings.Contains(to, bcc) {
-					buf.WriteString(field + ": " + to + "\r\n")
+					buf.WriteString(field)
+					buf.WriteString(": ")
+					buf.WriteString(to)
+					buf.WriteString("\r\n")
 				}
 			}
 		}
