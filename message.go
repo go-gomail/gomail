fc597c2cb8124b8e1ae818ebab75f5eaa93a1d10
pedromorgan <pedromorgan@gmail.com>
2017-12-07 02:18:49 +0000

    tidy more docs

--- message.go
+++ message.go
@@ -207,8 +207,9 @@ func newCopier(s string) func(io.Writer) error {
 	}
 }
 
-// AddAlternativeWriter adds an alternative part to the message. It can be
-// useful with the text/template or html/template packages.
+// AddAlternativeWriter- adds an alternative part to the message
+//
+// It can be  useful with the text/template or html/template packages.
 func (m *Message) AddAlternativeWriter(contentType string, f func(io.Writer) error, settings ...PartSetting) {
 	m.parts = append(m.parts, m.newPart(contentType, f, settings))
 }
@@ -227,13 +228,18 @@ func (m *Message) newPart(contentType string, f func(io.Writer) error, settings 
 	return p
 }
 
-// A PartSetting can be used as an argument in Message.SetBody,
-// Message.AddAlternative or Message.AddAlternativeWriter to configure the part
-// added to a message.
+// A PartSetting is used as an argument in
+//
+//  - Message.SetBody,
+//  - Message.AddAlternative
+//  - Message.AddAlternativeWriter
+//
+// to configure the part added to a message.
 type PartSetting func(*part)
 
-// SetPartEncoding sets the encoding of the part added to the message. By
-// default, parts use the same encoding than the message.
+// SetPartEncoding sets the encoding of the part added to the message.
+//
+// By default, parts use the same encoding than the message.
 func SetPartEncoding(e Encoding) PartSetting {
 	return PartSetting(func(p *part) {
 		p.encoding = e
@@ -250,7 +256,7 @@ func (f *file) setHeader(field, value string) {
 	f.Header[field] = []string{value}
 }
 
-// A FileSetting can be used as an argument in Message.Attach or Message.Embed.
+// A FileSetting is used as an argument in Message.Attach and Message.Embed.
 type FileSetting func(*file)
 
 // SetHeader is a file setting to set the MIME header of the message part that
@@ -277,8 +283,8 @@ func Rename(name string) FileSetting {
 // SetCopyFunc is a file setting to replace the function that runs when the
 // message is sent. It should copy the content of the file to the io.Writer.
 //
-// The default copy function opens the file with the given filename, and copy
-// its content to the io.Writer.
+// The default copy function opens the file with the given filename,
+// // then it copies its content to the io.Writer.
 func SetCopyFunc(f func(io.Writer) error) FileSetting {
 	return func(fi *file) {
 		fi.CopyFunc = f
@@ -313,12 +319,12 @@ func (m *Message) appendFile(list []*file, name string, settings []FileSetting) 
 	return append(list, f)
 }
 
-// Attach attaches the files to the email.
+// Attach - attaches the file to the email
 func (m *Message) Attach(filename string, settings ...FileSetting) {
 	m.attachments = m.appendFile(m.attachments, filename, settings)
 }
 
-// Embed embeds the images to the email.
+// Embed - embeds the images to the email
 func (m *Message) Embed(filename string, settings ...FileSetting) {
 	m.embedded = m.appendFile(m.embedded, filename, settings)
 }
